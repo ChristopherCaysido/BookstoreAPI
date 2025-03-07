b39cbf29fdcbd9d9f517dc5eb4a70f2d4d955448
ChristopherCaysido <christopher.caysido@gmail.com>
2025-03-07 22:43:40 +0800

    Add contact routes with basic GET and POST endpoints

---
+++ routes/contactRoutes.js
@@ -0,0 +1,13 @@
+const {Router} = require('express')
+
+const contactRouter = Router()
+
+contactRouter.get("/",(req,res)=>{
+    res.send("All contacts")
+})
+
+contactRouter.post("/",(req,res)=>{
+    res.send("Adding a contact")
+})
+
+
